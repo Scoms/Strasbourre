5af1bd234063e7efb4b410dfda9c7e31524e15f7
Scoms <wieser.laurent@gmail.com>
2013-12-03 16:42:38 +0100

    maj du core.php

--- app/Config/core.php
+++ app/Config/core.php
@@ -18,29 +18,6 @@
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 /**
- * Configure the cache used for general framework caching.  Path information,
- * object listings, and translation cache files are stored with this configuration.
- */
-Cache::config('_cake_core_', array(
-  'engine' => $engine,
-	'prefix' => $prefix . 'cake_core_',
-	'path' => '/tmp/', # New Path
-	'serialize' => ($engine === 'File'),
-	'duration' => $duration
-));
- 
-/**
- * Configure the cache for model and datasource caches.  This cache configuration
- * is used to store schema descriptions, and table listings in connections.
- */
-Cache::config('_cake_model_', array(
-	'engine' => $engine,
-	'prefix' => $prefix . 'cake_model_',
-	'path' => '/tmp/', # New Path
-	'serialize' => ($engine === 'File'),
-	'duration' => $duration
-));
-/**
  * CakePHP Debug Level:
  *
  * Production Mode:
