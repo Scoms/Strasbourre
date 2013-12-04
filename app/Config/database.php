c8c9d2e614203506442959e93b473bde40b5a254
Scoms <wieser.laurent@gmail.com>
2013-12-04 09:39:22 +0100

    x

---
+++ app/Config/database.php
@@ -0,0 +1,23 @@
+<?php
+class DATABASE_CONFIG {
+	/*public $default = array(
+        'datasource'  => 'Database/Mysql',
+        'persistent'  => false,
+        'host'        => 'localhost',
+        'login'       => 'Dev',
+        'password'    => 'password',
+        'database'    => 'Strasbourre',
+        'prefix'      => '',
+        'encoding'    => 'utf8'
+    );*/
+    public $default = array(
+        'datasource'  => 'Database/Mysql',
+        'persistent'  => false,
+        'host'        => 'ds057528.mongolab.com:57528',
+        'login'       => 'Dev',
+        'password'    => 'password',
+        'database'    => 'strasbourre',
+        'prefix'      => '',
+        'encoding'    => 'utf8'
+    );
+}
